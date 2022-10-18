b03730bb3431c64e301a743694aee77cc7c91852
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-10-18 22:43:20 +0900

    Categoryテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_134027) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
-  enable_extension "timescaledb"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "Category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
