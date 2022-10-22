5ebb4c847dc08b0c7c208618a5416cf59d28ecbf
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-10-22 22:59:27 +0900

    ガチャを回す部分の作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,10 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @meigen = Meigen.where(category_id: @category.id).sample  
+    session[:meigen] = @meigen
+  end
 end
