11e28b42abe4f3e3a27ba44740c0dc218eb5b5b7
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-11-23 19:05:59 +0900

    cloud function に接続する処理をcontrollerにべた書きするのをやめた

--- app/controllers/meigens_controller.rb
+++ app/controllers/meigens_controller.rb
@@ -7,38 +7,20 @@ class MeigensController < ApplicationController
     @meigen_id = session[:meigen_id]
     
     @is_session_existed = true
-    @is_original = false
 
     if @meigen_id.nil?
       @is_session_existed = false
     else
-      if @meigen_id == 0
-        @is_original = true
+      @is_original_meigen = false
 
-        url = URI.parse("https://us-central1-eighth-ridge-348103.cloudfunctions.net/meigen-recomment")
-          
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        http.read_timeout=120
-  
-        req = Net::HTTP::Post.new(url.request_uri)
-        req["content-Type"] = "application/json"
-        body = {
-            "new_meigen" => ""
-        }.to_json
-        req.body = body
-  
+      if @meigen_id == 0
+        @is_original_meigen = true
         begin
-            res = http.request(req)
-            results = JSON.parse(res.body) 
-            puts results
-            @meigen = results
-            
+          @original_meigen_body = Meigen.fetch_original_meigen
         rescue => exception
-            puts exception.message
-            @is_session_existed = false
+          @is_session_existed = false
         end
+
       else
         @meigen = Meigen.find(@meigen_id)
       end
@@ -49,36 +31,11 @@ class MeigensController < ApplicationController
   def async_find_meigen_by_schedule
     
     Thread.start do
-      
-      today_all_schedules = []
-      today_schedules = []
-      today_schedules.push(params[:schedule])
-      today_all_schedules.push(today_schedules)
-
-      url = URI.parse("https://us-central1-eighth-ridge-348103.cloudfunctions.net/meigen-recomment")
-          
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      http.read_timeout=120
-
-      req = Net::HTTP::Post.new(url.request_uri)
-      req["content-Type"] = "application/json"
-      body = {
-          "schedules" => today_all_schedules
-      }.to_json
-      req.body = body
-
-      meigen_body_for_schedule = ""
 
       begin
-        res = http.request(req)
-        results = JSON.parse(res.body) 
-        puts results
-        meigen_body_for_schedule = results[0]
-        @@meigen_bodies_for_schedule.store(params[:schedule], meigen_body_for_schedule)
+        @@meigen_bodies_for_schedule.store(params[:schedule], Meigen.fetch_meigen_by_schedule(params[:schedule]))
+        puts "完了"
       rescue => exception
-          puts "errorrrr"
           puts exception.message
       end
 
@@ -88,6 +45,7 @@ class MeigensController < ApplicationController
   end
 
   def set_meigen_for_schedule_to_session
+    puts "取り出し開始"
     meigen_model_for_schedule = Meigen.find_by(body: @@meigen_bodies_for_schedule[params[:schedule]])
     session[:meigen_id] = meigen_model_for_schedule.id
     render json: ""
--- app/models/Meigen.rb
+++ app/models/Meigen.rb
@@ -1,6 +1,67 @@
+require 'net/http'
+
 class Meigen < ApplicationRecord
 
     belongs_to :gacha
     belongs_to :author
+
+    def self.fetch_original_meigen
+        url = URI.parse("https://us-central1-eighth-ridge-348103.cloudfunctions.net/meigen-recomment")
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.read_timeout=120
   
+        req = Net::HTTP::Post.new(url.request_uri)
+        req["content-Type"] = "application/json"
+        body = {
+            "new_meigen" => ""
+        }.to_json
+        req.body = body
+        begin
+            res = http.request(req)
+            original_meigen_body = JSON.parse(res.body) 
+            puts original_meigen_body
+            return original_meigen_body
+            
+        rescue => exception
+            puts exception.message
+            raise Exception.new exception.message
+        end
+    end
+
+    def self.fetch_meigen_by_schedule(schedule_name)
+        today_all_schedules = []
+        today_schedules = []
+        today_schedules.push(schedule_name)
+        today_all_schedules.push(today_schedules)
+
+        url = URI.parse("https://us-central1-eighth-ridge-348103.cloudfunctions.net/meigen-recomment")
+            
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.read_timeout=120
+
+        req = Net::HTTP::Post.new(url.request_uri)
+        req["content-Type"] = "application/json"
+        body = {
+            "schedules" => today_all_schedules
+        }.to_json
+        req.body = body
+
+        meigen_body_for_schedule = ""
+
+        begin
+            res = http.request(req)
+            results = JSON.parse(res.body) 
+            puts results
+            meigen_body_for_schedule = results[0]
+            return meigen_body_for_schedule
+        rescue => exception
+            puts exception.message
+
+            raise Exception.new exception.message
+        end
+    end
 end
