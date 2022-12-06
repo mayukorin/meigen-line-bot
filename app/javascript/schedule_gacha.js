79b11312bb46289bc5b6ff788b7abae1bd949fac
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-12-06 16:08:40 +0900

    ガチャから取り出すに名前を変えた

--- app/javascript/schedule_gacha.js
+++ app/javascript/schedule_gacha.js
@@ -21,7 +21,7 @@ gacha.addEventListener('click',function(){
         .then(() => {
             animation.pause();
             const target = document.getElementById("to-result");
-            target.innerText = "ガチャ結果を見る";
+            target.innerText = "ガチャから取り出す";
             target.classList.remove("disabled");
         });
         clickCnt += 1;
