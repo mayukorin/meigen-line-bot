9b230a370669206b915c1177afac6068a445a3d8
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2023-01-04 01:08:22 +0900

    不要なcontrollerを消した

--- test/controllers/gachas_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class GachasControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get gachas_index_url
-    assert_response :success
-  end
-end
--- test/controllers/meigens_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MeigensControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get meigens_show_url
-    assert_response :success
-  end
-end
