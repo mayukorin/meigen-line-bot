2301756d439c3119bd5b183d76113267abd38b57
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2023-01-09 16:34:19 +0900

    色々リファクタ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -10,20 +10,16 @@ class LinebotController < ApplicationController
     BASIC_PLAN_FOR_ENGLISH = 'basic'
     PREIUM_PLAN_FOR_ENGLISH = 'premium'
 
-    def self.set_schedule_meigen_request_user(userId, schedule)
+    def self.set_schedule_meigen_request_user(userId:, schedule:)
         @schedule_meigen_request_users.store(userId, schedule)
     end
 
-    def self.get_schedule_for_request_user(userId)
+    def self.get_schedule_for_request_user(userId:)
         @schedule_meigen_request_users[userId]
     end
 
-    def self.remove_schedule_meigen_request_user(userId)
+    def self.remove_schedule_meigen_request_user(userId:)
         @schedule_meigen_request_users.delete(userId)
-    end
-
-    def self.is_schedule_meigen_request_user(userId)
-        @schedule_meigen_request_users.include?(userId)
     end
 
     def client
@@ -31,6 +27,57 @@ class LinebotController < ApplicationController
             config.channel_id = ENV["LINE_CHANNEL_ID"]
             config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
             config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+        }
+    end
+
+    def mode_question_message
+        mode_question_message = {
+            type: 'template',
+            altText: '名言を取得するモードを選択させる',
+            template: {
+                type: 'confirm',
+                text: "モードを選んでください！\n・ベーシック：30秒ほどで予定に合う名言を選びます．とりあえず名言がほしい！という方にお勧めです．\n・プレミアム：1分30秒ほどじっくりかけて名言を選びます．その分より良い名言を選べるかもしれません．時間をかけてもいいから，自分に合った名言がほしいという方にお勧めです"．,
+                actions: [
+                    {
+                        type: 'postback',
+                        label: BASIC_PLAN_FOR_JAPANESE,
+                        data:  BASIC_PLAN_FOR_ENGLISH,
+                        text: BASIC_PLAN_FOR_JAPANESE
+                    },
+                    {
+                        type: 'postback',
+                        label: PREIUM_PLAN_FOR_JAPANESE,
+                        data: PREIUM_PLAN_FOR_ENGLISH,
+                        text: PREIUM_PLAN_FOR_JAPANESE
+                    }
+                ]
+            }
+        }
+    end
+
+    def is_basic_or_premium_mode(selected_mode:)
+        if selected_mode != PREIUM_PLAN_FOR_ENGLISH and selected_mode != BASIC_PLAN_FOR_ENGLISH
+            return false
+        end
+        return true
+    end
+
+    def most_fit_meigen_for_schedule_message(selected_mode:, schedule:)
+        begin
+            case selected_mode
+            when PREIUM_PLAN_FOR_ENGLISH
+                meigen_body = OriginalAndScheduleMeigenFetcher.fetch_meigen_body_by_schedule_from_cloud_function(schedule: schedule, mode: PREIUM_PLAN_FOR_ENGLISH)
+            when BASIC_PLAN_FOR_ENGLISH
+                meigen_body = OriginalAndScheduleMeigenFetcher.fetch_meigen_body_by_schedule_from_cloud_function(schedule: schedule, mode: BASIC_PLAN_FOR_ENGLISH)
+            end
+            message_text = schedule+"，頑張ってください！\nそんなあなたに贈る名言は「"+meigen_body+"」です！"
+        rescue => exception
+            message_text = "申し訳ありません．名言を選べませんでした．少し時間をおいてからもう一度お試しください．"
+        end
+
+        most_fit_meigen_for_schedule_message  = {
+            type: 'text',
+            text: message_text
         }
     end
 
@@ -42,62 +89,27 @@ class LinebotController < ApplicationController
             when Line::Bot::Event::Message
                 case event.type
                 when Line::Bot::Event::MessageType::Text
-                    userId = event['source']['userId']
-
                     if event.message['text'] == BASIC_PLAN_FOR_JAPANESE || event.message['text'] == PREIUM_PLAN_FOR_JAPANESE
                         puts 'this message is mode answer message. so, ignore'
                         return
                     end
 
-                    schedule = event.message['text']
-                    LinebotController.set_schedule_meigen_request_user(userId, schedule)
-
-                    mode_question_message = {
-                        type: 'template',
-                        altText: '名言を取得するモードを選択させる',
-                        template: {
-                            type: 'confirm',
-                            text: '名言を取得するモードを選んでください！',
-                            actions: [
-                                {
-                                    type: 'postback',
-                                    label: BASIC_PLAN_FOR_JAPANESE,
-                                    data:  BASIC_PLAN_FOR_ENGLISH,
-                                    text: BASIC_PLAN_FOR_JAPANESE
-                                },
-                                {
-                                    type: 'postback',
-                                    label: PREIUM_PLAN_FOR_JAPANESE,
-                                    data: PREIUM_PLAN_FOR_ENGLISH,
-                                    text: PREIUM_PLAN_FOR_JAPANESE
-                                }
-                            ]
-                        }
-                    }
+                    LinebotController.set_schedule_meigen_request_user(userId: event['source']['userId'], schedule: event.message['text'])
 
                     client.reply_message(event['replyToken'], mode_question_message)
                 end
             when Line::Bot::Event::Postback
-                schedule = LinebotController.get_schedule_for_request_user(event['source']['userId'])
-
-                begin
-                    case event['postback']['data']
-                    when PREIUM_PLAN_FOR_ENGLISH
-                        meigen_body = OriginalAndScheduleMeigenFetcher.fetch_meigen_body_by_schedule_from_cloud_function(schedule, PREIUM_PLAN_FOR_ENGLISH)
-                    when BASIC_PLAN_FOR_ENGLISH
-                        meigen_body = OriginalAndScheduleMeigenFetcher.fetch_meigen_body_by_schedule_from_cloud_function(schedule, BASIC_PLAN_FOR_ENGLISH)
-                    end
-                    message_text = schedule+"，頑張ってください！\nそんなあなたに贈る名言は「"+meigen_body+"」です！"
-                rescue => exception
-                    message_text = "申し訳ありません．名言を選べませんでした．少し時間をおいてからもう一度お試しください．"
+                selected_mode = event['postback']['data']
+                unless is_basic_or_premium_mode(selected_mode: selected_mode)
+                    puts 'this message is sent by invalid route. so, ignore'
+                    return
                 end
 
-                message = {
-                    type: 'text',
-                    text: message_text
-                }
-                client.reply_message(event['replyToken'], message)
-                LinebotController.remove_schedule_meigen_request_user(userId)
+                userId = event['source']['userId']
+                schedule = LinebotController.get_schedule_for_request_user(userId: userId)
+                client.reply_message(event['replyToken'], most_fit_meigen_for_schedule_message(selected_mode: selected_mode, schedule: schedule))
+
+                LinebotController.remove_schedule_meigen_request_user(userId: userId)
             end
         end
         "OK"
